443bc10adff6bcf8c3cbce5634e616806835a6d0
JoshuaLopez0378 <joshuaalbertlopez0378@gmail.com>
2025-04-28 19:07:57 +0800

    fix: quick fix for handling non-existing dir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import path, listdir, mkdir
 from pydub import AudioSegment
+import threading
 
 root_path = "E:/Games/x64/soundlines/"
 to_convert_path = str(
@@ -26,29 +27,32 @@ def convert_mp3_to_wav_mono(input_folder, output_folder, d_folder, base_char_nam
         split_f = folder.split(" ")
         print(len(split_f))
         id_f = 1
-        d_sub_folder = listdir(input_folder + "/" + folder)
-        print("Current Folder: ", folder)
-        print("Input Files List:", d_sub_folder)
-        print()
-        for file in d_sub_folder:
-            if len(split_f) > 1:
-                folder_new = split_f[0].capitalize() + split_f[1].capitalize()
-            else:
-                folder_new = folder.capitalize()
-            sound = AudioSegment.from_mp3(input_folder + "/" + folder + "/" + file)
-            sound = sound.set_channels(1)
-            sound.export(
-                "out_results/"
-                + f"{base_char_name}/"
-                + base_char_name
-                + folder_new
-                + str(id_f)
-                + ".wav",
-                format="wav",
-            )
+        try:
+            d_sub_folder = listdir(input_folder + "/" + folder)
+            print("Current Folder: ", folder)
+            print("Input Files List:", d_sub_folder)
+            print()
+            for file in d_sub_folder:
+                if len(split_f) > 1:
+                    folder_new = split_f[0].capitalize() + split_f[1].capitalize()
+                else:
+                    folder_new = folder.capitalize()
+                sound = AudioSegment.from_mp3(input_folder + "/" + folder + "/" + file)
+                sound = sound.set_channels(1)
+                sound.export(
+                    "out_results/"
+                    + f"{base_char_name}/"
+                    + base_char_name
+                    + folder_new
+                    + str(id_f)
+                    + ".wav",
+                    format="wav",
+                )
 
-            print("Output To: ", base_char_name + input_folder + str(id_f) + ".wav")
-            id_f += 1
+                print("Output To: ", base_char_name + input_folder + str(id_f) + ".wav")
+                id_f += 1
+        except:
+            continue
     return "OK"
 
 
